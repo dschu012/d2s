b0b8cfdc4ea614c4cf7845e9ed2ceae12ecea5c8
Mathias Gamperl <m.gamperl@gmail.com>
2021-05-03 21:55:31 +0200

    added some more lines to be ignored by prettier

--- tests/d2/attributes.spec.ts
+++ tests/d2/attributes.spec.ts
@@ -8,45 +8,8 @@ import { constants } from "../../src/data/versions/96_constant_data";
 describe("attributes", () => {
   it("should read", async () => {
     const d2s = {} as types.ID2S;
-    const buffer = new Uint8Array([
-      103,
-      102,
-      0,
-      60,
-      8,
-      160,
-      128,
-      0,
-      10,
-      6,
-      100,
-      96,
-      0,
-      224,
-      6,
-      28,
-      0,
-      184,
-      1,
-      8,
-      0,
-      20,
-      64,
-      2,
-      0,
-      5,
-      160,
-      0,
-      128,
-      11,
-      44,
-      0,
-      224,
-      2,
-      12,
-      2,
-      255,
-      1,
+    // prettier-ignore
+    const buffer = new Uint8Array([103,102,0,60,8,160,128,0,10,6,100,96,0,224,6,28,0,184,1,8,0,20,64,2,0,5,160,0,128,11,44,0,224,2,12,2,255,1,
     ]);
     const reader = new BinaryReader(buffer).SetLittleEndian();
     await readAttributes(d2s, reader, constants);
@@ -69,47 +32,8 @@ describe("attributes", () => {
       level: 1,
     } as types.IAttributes;
     const bytes = await writeAttributes(d2s, constants);
-    expect(bytes).to.eql(
-      new Uint8Array([
-        103,
-        102,
-        0,
-        60,
-        8,
-        160,
-        128,
-        0,
-        10,
-        6,
-        100,
-        96,
-        0,
-        224,
-        6,
-        28,
-        0,
-        184,
-        1,
-        8,
-        0,
-        20,
-        64,
-        2,
-        0,
-        5,
-        160,
-        0,
-        128,
-        11,
-        44,
-        0,
-        224,
-        2,
-        12,
-        2,
-        255,
-        1,
-      ])
+    // prettier-ignore
+    expect(bytes).to.eql(new Uint8Array([103,102,0,60,8,160,128,0,10,6,100,96,0,224,6,28,0,184,1,8,0,20,64,2,0,5,160,0,128,11,44,0,224,2,12,2,255,1,])
     );
   });
 });

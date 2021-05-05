8934204f0028cf610abf5f2dc05fd8dc000f170d
unknown <dschu012@gmail.com>
2021-05-05 12:06:39 -0400

    expose attribute enhancer

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,6 @@ export * from "./d2/d2s";
 export { readHeader, readHeaderData, writeHeader, writeHeaderData, fixHeader } from "./d2/header";
 export { readAttributes, writeAttributes } from "./d2/attributes";
 export { readSkills, writeSkills } from "./d2/skills";
+export { enhanceAttributes, enhanceItem, enhanceItems, enhancePlayerAttributes } from "./d2/attribute_enhancer";
 export * from "./data/parser";
 export * as types from "./d2/types";
